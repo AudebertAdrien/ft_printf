edb00fc7453c1c21ff529a174cc16b1d0a297b6e
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-03-02 10:06:14 +0100

    put c and i

--- Makefile
+++ Makefile
@@ -1,15 +1,18 @@
 NAME		= a.out
 
-CC		= gcc
+CC			= gcc
 CFLAGS		= -Wall -Wextra
 DLEAK		= -fsanitize=address
+DEPS		= libft.h
 
-SRC		= main.c
+SRC		= main.c \
+		  ft_putchar_fd.c \
+		  ft_putnbr_fd.c
 
 all: $(NAME)
 
 $(NAME): 
-	$(CC) -g $(CFLAGS) $(SRC)
+	$(CC) -g $(CFLAGS) -I.$(DEPS) $(SRC)
 
 clean:
 	rm -f $(OBJ)
--- main.c
+++ main.c
@@ -6,53 +6,50 @@
 /*   By: motoko <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 15:08:21 by motoko            #+#    #+#             */
-/*   Updated: 2023/03/01 17:00:20 by motoko           ###   ########.fr       */
+/*   Updated: 2023/03/02 10:05:26 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
+#include "libft.h"
 
 int	ft_printf(const char *format, ...)
 {
-	int	res;
+	int	count;
 	va_list arg_lst;
 
 	va_start(arg_lst, format);
-
+	count = 0;
 	while (*format) {
 		if (*format == '%') {
 			format++;
 			if (*format == 'i') {
-				//printf("i\n");	
-				res = va_arg(arg_lst, int);
-				//printf("res %d\n", res);
-				write(1, &format ,1);
+				ft_putnbr_fd(va_arg(arg_lst, int), 1);
 			}
 			if (*format == 'c') {
-				//printf("c\n");
-				res = va_arg(arg_lst, int);
-				//printf("res %d\n", res);
-				write(1, &format ,1);
+				ft_putchar_fd(va_arg(arg_lst, int), 1);
 			}
 		}
 		else {
-			write(1, format, 1);
+			ft_putchar_fd(*format, 1);
 		}
+		count++;
 		format++;	
 	}
 	va_end(arg_lst);
-	return (0);
+	return (count);
 }
 
 int main (void)
 {
 	int	i;
+	int	res;
 	char	c;
 
+	res = 0;
 	c = 'W';
 	i = 3;
-	ft_printf("Hello %corld 202%i", c, i);
+	res = ft_printf("Hello %corld 202%i", c, i);
+//	printf("\n");
+//	printf("res : %d\n", res);
 	return 0;
 }

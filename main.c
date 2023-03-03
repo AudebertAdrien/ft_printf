bd0c3b06fdc026129175481aab15b47c60e5b848
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-03-03 11:10:41 +0100

    maim

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: motoko <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 15:08:21 by motoko            #+#    #+#             */
-/*   Updated: 2023/03/03 09:11:55 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/03/03 09:24:30 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,43 @@ int	ft_printf(const char *format, ...);
 
 int main (void)
 {
-//<<<<<<< HEAD
-//	int	count;
-//	
-//	count = 0;
-//	count = ft_printf("Hello %corld 202%i", 'W', 3);
-//	printf("\n%d\n", count);
-//	count = printf("Hello %corld 202%i", 'W', 3);
-//	printf("\n%d\n", count);
-//
-//	count = ft_printf("%s", "toto");
-//	printf("\n%d\n", count);
-//	count = printf("%s", "toto");
-//	printf("\n%d\n", count);
-//
-//	count = ft_printf("%d", 49);
-//	printf("\n%d\n", count);
-//	count = printf("%d", 49);
-//	printf("\n%d\n", count);
-//=======
-	ft_printf("Hello %corld 202%i", 'W', 3);
-	printf("\n");
-	printf("Hello %corld 202%i", 'W', 3);
+	int	count;
+	
+	count = 0;
+
+	printf("\n//////////////////\n");
+	count = ft_printf("Hello %corld 202%i", 'W', 30);
+	printf("\n%d\n", count);
+	count = printf("Hello %corld 202%i", 'W', 30);
+	printf("\n%d", count);
+
+	printf("\n//////////////////\n");
+	
+	count = ft_printf("%i", 30);
+	printf("\n%d\n", count);
+	count = printf("%i", 30);
+	printf("\n%d", count);
 
 	printf("\n//////////////////\n");
 
-	ft_printf("%s", "toto");
-	printf("\n");
-	printf("%s", "toto");
+	count = ft_printf("%s", "toto");
+	printf("\n%d\n", count);
+	count = printf("%s", "toto");
+	printf("\n%d", count);
 
 	printf("\n//////////////////\n");
 
 	ft_printf("%d", 49);
-	printf("\n");
+	count = printf("\n%d\n", count);
 	printf("%d", 49);
+	count = printf("\n%d", count);
 
 	printf("\n//////////////////\n");
 	
-	ft_printf("%d", 118);
-	printf("\n");
-	printf("%d", 118);
+	count = ft_printf("%d", 118);
+	printf("\n%d\n", count);
+	count = printf("%d", 118);
+	printf("\n%d", count);
 	
 	printf("\n//////////////////\n");
 
@@ -64,9 +61,10 @@ int main (void)
 	
 	printf("\n//////////////////\n");
 
-	ft_printf("%x", 42);
-	printf("\n");
-	printf("%x", 42);
+	count = ft_printf("%x", 42);
+	printf("\n%d\n", count);
+	count = printf("%x", 42);
+	printf("\n%d", count);
 
 	printf("\n//////////////////\n");
 	return 0;

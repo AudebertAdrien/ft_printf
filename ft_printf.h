f4271af98e3efc75956d5c20c77ef286f2fe8f4b
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-03-07 13:56:05 +0100

    nn .h

--- ft_printf.h
+++ ft_printf.h
@@ -6,27 +6,25 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 13:43:01 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/03/07 13:47:02 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/03/07 13:55:55 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
 # include <stdarg.h>
 
-
 // Printf
-int	ft_printf(const char *format, ...);
-int	ft_printnbr_base(unsigned int nbr, char *base);
-int	ft_printnbr_int(int n, int count);
-int	ft_printnbr_unsigned(size_t n, int count);
-int	ft_printnbr_ptr(unsigned long nbr);
-int	ft_printchar(char c);
-int	ft_printstr(const char *c);
+int		ft_printf(const char *format, ...);
+int		ft_printnbr_base(unsigned int nbr, char *base);
+int		ft_printnbr_int(int n, int count);
+int		ft_printnbr_unsigned(size_t n, int count);
+int		ft_printnbr_ptr(unsigned long nbr);
+int		ft_printchar(char c);
+int		ft_printstr(const char *c);
 size_t	ft_strlen(const char *str);
 #endif
